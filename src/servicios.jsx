9cbd7e96a23cc5c2ed02b2bec30851e415ae95a6
Angel <angelernesto307@gmail.com>
2024-04-22 22:16:13 -0500

    servicio 2

--- src/servicios.jsx
+++ src/servicios.jsx
@@ -10,9 +10,20 @@ export const listarEntidad=async ({entidad='mascotas'})=>{
     }
 
 }
-export const crearEntidad=async ({entidad='mascotas',objeto={}})=>{
+export const crearEntidad=async ({entidad='mascotas',objeto={},method='POST',idObjeto=null})=>{
     try {
-        const respuesta=await fetch(`${api_url}/${entidad}`)
+        let url=`${api_url}/${entidad}`;
+        if(method=='PUT' && idObjeto){
+            url+=`/${idObjeto}`;
+        }
+        const respuesta=await fetch(url,{
+            method,
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(objeto),
+            mode:'cors',
+        })
         const datos=await respuesta.json();
         return datos;
     } catch (error) {
